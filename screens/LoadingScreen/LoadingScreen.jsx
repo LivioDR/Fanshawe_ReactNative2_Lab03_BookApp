317bb90bc0652cb2dceefaaeea80ba219bb1f520
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-07 14:50:43 -0500

    LoadingScreen styling completed

--- screens/LoadingScreen/LoadingScreen.jsx
+++ screens/LoadingScreen/LoadingScreen.jsx
@@ -1,11 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
 // TODO: set styles for this screen
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        marginVertical: 10,
+    },
+})
 
 const LoadingScreen = () => {
     return(
-        <View>
-            <Text>Loading...</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Loading</Text>
+            <ActivityIndicator size={'large'}/>
         </View>
     )
 }
