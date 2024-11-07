c5d41b07f9fb40037fcdbdcf3becd2e182f9edae
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-07 14:03:24 -0500

    created Loading Screen

---
+++ screens/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+// TODO: set styles for this screen
+
+const LoadingScreen = () => {
+    return(
+        <View>
+            <Text>Loading...</Text>
+        </View>
+    )
+}
+export default LoadingScreen
