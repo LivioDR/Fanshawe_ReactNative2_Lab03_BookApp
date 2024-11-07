c59f95b41169ac2e2a53a8fcd5698d6ce5ecd6ac
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-06 21:46:22 -0500

    created screen placeholders

---
+++ screens/BorrowedScreen/BorrowedScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const BorrowedScreen = () => {
+    return(
+        <View style={{flex: 1, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center',}}>
+            <Text>Borrowed Screen</Text>
+        </View>
+    )
+}
+export default BorrowedScreen
---
+++ screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const HomeScreen = () => {
+    return(
+        <View style={{flex: 1, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center',}}>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+export default HomeScreen
