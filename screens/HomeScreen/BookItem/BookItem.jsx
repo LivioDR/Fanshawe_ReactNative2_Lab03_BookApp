d992dc605178c7e6a8fa4fea6fd7a27f3323aa4d
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-06 22:45:00 -0500

    added TouchableHighlight wrapper

--- screens/HomeScreen/BookItem/BookItem.jsx
+++ screens/HomeScreen/BookItem/BookItem.jsx
@@ -1,7 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 const styles = StyleSheet.create({
+    touchWrapper: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
     container: {
         width: '95%',
         display: 'flex',
@@ -29,15 +37,23 @@ const styles = StyleSheet.create({
 const BookItem = ({data}) => {
 
     return(
-        <View style={styles.container}>
-            <Text style={styles.author}>{data.author}</Text>
-            <Text style={styles.title}>{data.title}</Text>
-            {
-                data.borrowed ? 
-                <Ionicons name="book" size={18} color={'black'} style={styles.icon}/>
-                : <Text style={styles.icon}></Text>
-            }
-        </View>
+        <TouchableHighlight
+            style={styles.touchWrapper}
+            underlayColor={'#D7AE47'}
+            onPress={()=>console.log(data.title)}
+        >
+            <View style={styles.container}>
+                <>
+                <Text style={styles.author}>{data.author}</Text>
+                <Text style={styles.title}>{data.title}</Text>
+                {
+                    data.borrowed ? 
+                    <Ionicons name="book" size={18} color={'black'} style={styles.icon}/>
+                    : <Text style={styles.icon}></Text>
+                }
+                </>
+            </View>
+        </TouchableHighlight>
     )
 }
 export default BookItem
